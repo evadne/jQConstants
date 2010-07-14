2b8ef0a844c5b09cd1a329ce5ec3ab9623634963
Evadne Wu <ev@monoceroi.com>
2010-07-14 18:01:10 +0800

    initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+#	`lib.jQuery.constants.js`
+
+You **will** forget what `return true;` does in a `jQuery.each()` loop.  But you can always understand what `return jQuery.kLoopTerminationStopFutureIterations;` does.
+
+`jQuery.constants` brings constants that are meaningful to you, which get translated to something meaningful to jQuery at run time.
+
+
+
+
---
+++ lib.jQuery.constants.js
@@ -0,0 +1,34 @@
+//	lib.jQuery.constants.js
+//	Evadne Wu at Iridia, 2010
+
+
+
+
+
+	(function (jQuery) {
+	
+		if (jQuery === undefined) return;
+		jQuery.mnConstants = true;
+	
+	//	$.extend
+		
+		jQuery.kDeepCopyEnabled = true;
+		jQuery.kDeepCopyDisabled = false;
+		
+	
+	//	$.each
+		
+		jQuery.kLoopTerminationStopFutureIterations = false;
+		jQuery.kLoopTerminationSkipIteration = true;
+	
+	
+	})(jQuery);
+
+
+
+
+
+
+
+
+
